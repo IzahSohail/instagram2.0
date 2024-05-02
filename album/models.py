1f0cb3d9d26e5b2bf9a89a3cffd8f0cda2d1b4a5
izah2587 <izahs2003@gmail.com>
2024-05-02 14:40:58 +0400

    creating albums/uploading photos features

--- album/models.py
+++ album/models.py
@@ -6,6 +6,7 @@ class Album(models.Model):
     album_id = models.AutoField(primary_key=True)
     album_name = models.CharField(max_length=50)
     date_of_creation = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.album_name
@@ -15,7 +16,7 @@ class Photo(models.Model):
     photo_id = models.AutoField(primary_key=True)
     photo_data = models.BinaryField()
     caption = models.CharField(max_length=50)
-    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
