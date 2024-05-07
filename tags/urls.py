b128a8ccd3cf8d7a86eabae9161a5ca6c3cb2481
Your Name <you@example.com>
2024-05-07 22:14:56 +0400

    quick update

--- tags/urls.py
+++ tags/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('my_tags/<int:tag_id>/', views.my_tags, name='my_tags'),
     path('all_tags/<int:tag_id>/', views.all_tags, name='all_tags'),
     path('most_popular/', views.most_popular, name='most_popular'),
+    path('tag_search/', views.tag_search, name='tag_search'),
 ]
--- tags/views.py
+++ tags/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
 
 from album.models import Album, Photo, Photo_likes
 from .models import Tag, PhotoTagMapping
@@ -97,7 +98,6 @@ def all_tags(request, tag_id):
         total_likes = Photo_likes.objects.filter(photo=photo).count()
         
         owner = photo.owner
-        print(owner)
 
         photos_data.append({
             'photo_id': photo.photo_id, 
@@ -133,18 +133,73 @@ def most_popular(request):
 
     return render(request, 'tags/most_popular.html', {'tags': tags})
 
-
+@csrf_exempt
 def tag_search(request, tags):
 
-    execution_block = [-1]
-    for element in elements:
-        raw_query = """
-        SELECT {} AS Map.tag_id
-        INTERSECTION
-        SELECT          Map.tag_id
-        FROM            tags_phototagmapping AS Map, tags_tag AS Tag
-        WHERE           Map.tag_id = Tag.tag_id
-        """.format(execution_block)
-        execution_block.append(raw_query)
+    if request.method == 'POST':
+        pass
 
-    return render(request, 'tags/tag_search.html', {'photos': photos})
+    else:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                            CREATE TEMPORARY TABLE searched_tags (
+                            tag_id INTEGER PRIMARY KEY)
+                                                            """)
+
+            for tag in tags:   
+                cursor.execute("""
+                                INSERT INTO searched_tags
+                                VALUES ((SELECT tag_id FROM tags_tag WHERE description = {}))
+                                """.format(tag))
+
+            raw_query = """
+            SELECT  Photos.photo_id
+            FROM	ALBUM_PHOTOS AS Photos
+            WHERE	NOT EXISTS (
+                                SELECT	SEARCHED_TAGS.tag_id
+                                FROM 	SEARCHED_TAGS
+                                EXCEPT
+                                SELECT	Mapping.tag_id
+                                FROM	TAGS_PHOTOTAGMAPPING AS Mapping
+                                WHERE	Photos.photo_id = Mapping.photo_id);
+            """
+
+            cursor.execute(raw_query)
+            rows = cursor.fetchall()
+
+            photos=[]
+            for row in rows:
+                photo_id = rows[0]
+                photo = Photo.objects.get(photo_id=photo_id)
+                photos.append(photo)
+
+            photos_data = []
+            for photo in photos:
+                encoded_photo = base64.b64encode(photo.photo_data).decode('utf-8')
+                photo_src = f"data:image/*;base64,{encoded_photo}"
+
+                #tags for each photo
+                tags=[]
+                photo_tag_map = PhotoTagMapping.objects.filter(photo=photo)
+                for mapping in photo_tag_map:
+                    tags.append(mapping.tag)
+
+                #comments for each photo
+                comments = Comment.objects.filter(photo=photo)
+
+                # Count likes for each photo
+                total_likes = Photo_likes.objects.filter(photo=photo).count()
+                
+                owner = photo.owner
+
+                photos_data.append({
+                    'photo_id': photo.photo_id, 
+                    'photo_data': photo_src,
+                    'owner': owner,
+                    'caption': photo.caption,
+                    'total_likes': total_likes,
+                    'comments': comments,
+                    'tags': tags
+                })
+
+        return render(request, 'tags/tag_search.html', {'photos': photos_data})
